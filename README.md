abb92f524b3e8f85b28e97a304b6ec75567ce4f0
Manoel <gal18021@byui.edu>
2021-11-04 19:12:16 -0600

    Login db

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Th_-Catipult-Game
 
 ## Code Formating
-To keep things orginized, here are the formating conventions for the code:
+To keep things organized, here are the formating conventions for the code:
 - Variables:  all lowercase using underscores to seperate words.  (my_varible)
 - Classes: Prefrebly one word, capitilized.  (Engine)
 
 ## Filestructure
-The game is composed primarily of three main files, with all other assets contained within the appropriate "Assets" sub folder.  The Core.py file conains the main game, while the game_settings.csv file works as long term storage for the game.  The game_save.csv file keeps track to run sespific elements of the game, tracking user informaion for future use.
+The game is composed primarily of three main files, with all other assets contained within the appropriate "Assets" sub folder.  The Core.py file conains the main game, while the game_settings.csv file works as long term storage for the game.  The game_save.csv file keeps track to run specific elements of the game, tracking user information for future use.
 
 ## core.py Structure
 The core.py file is structured into two main components, the Engine and the Interface.
--- core.py
+++ core.py
@@ -3,6 +3,103 @@
 import arcade
 import math
 import csv
+from tkinter import *
+import sqlite3
+
+
+root = Tk()
+root.title("BYUI - CSE 310 team 4")
+width = 400
+height = 280
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+
+#==============================VARIABLES======================================
+USERNAME = StringVar()
+PASSWORD = StringVar()
+ 
+#==============================FRAMES=========================================
+Top = Frame(root, bd=2,  relief=RIDGE)
+Top.pack(side=TOP, fill=X)
+Form = Frame(root, height=200)
+Form.pack(side=TOP, pady=20)
+ 
+#==============================LABELS=========================================
+lbl_title = Label(Top, text = "CATIPULT GAME", font=('arial', 35))
+lbl_title.pack(fill=X)
+lbl_username = Label(Form, text = "Username:", font=('arial', 14), bd=15)
+lbl_username.grid(row=0, sticky="e")
+lbl_password = Label(Form, text = "Password:", font=('arial', 14), bd=15)
+lbl_password.grid(row=1, sticky="e")
+lbl_text = Label(Form)
+lbl_text.grid(row=2, columnspan=2)
+ 
+#==============================ENTRY WIDGETS==================================
+username = Entry(Form, textvariable=USERNAME, font=(14))
+username.grid(row=0, column=1)
+password = Entry(Form, textvariable=PASSWORD, show="*", font=(14))
+password.grid(row=1, column=1)
+ 
+#==============================BUTTON WIDGETS=================================
+Login = 1
+btn_login = Button(Form, text="Login", width=45, command=Login)
+btn_login.grid(pady=25, row=3, columnspan=2)
+btn_login.bind('<Return>', Login)
+
+#==============================METHODS========================================
+def Database():
+    global conn, cursor
+    conn = sqlite3.connect("pythontut.db")
+    cursor = conn.cursor()
+    cursor.execute("")       
+    cursor.execute("")
+    if cursor.fetchone() is None:
+        cursor.execute("")
+        conn.commit()
+
+def Login(event=None):
+    Database()
+    if USERNAME.get() == "" or PASSWORD.get() == "":
+        lbl_text.config(text="Please complete the required field!", fg="red")
+    else:
+        cursor.execute("SELECT * FROM `member` WHERE `username` = ? AND `password` = ?", (USERNAME.get(), PASSWORD.get()))
+        if cursor.fetchone() is not None:
+            HomeWindow()
+            USERNAME.set("")
+            PASSWORD.set("")
+            lbl_text.config(text="")
+        else:
+            lbl_text.config(text="Invalid username or password", fg="red")
+            USERNAME.set("")
+            PASSWORD.set("")   
+    cursor.close()
+    conn.close()
+ 
+def HomeWindow():
+    global Home
+    root.withdraw()
+    Home = Toplevel()
+    Home.title("WELCOME TO CATIPULT GAME 2.0")
+    width = 600
+    height = 500
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    root.resizable(0, 0)
+    Home.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    lbl_home = Label(Home, text="Successfully Login!", font=('times new roman', 20)).pack()
+    btn_back = Button(Home, text='Back', command=Back).pack(pady=20, fill=X)
+ 
+def Back():
+    Home.destroy()
+    root.deiconify()
+
+
 
 
 class Engine:
@@ -79,7 +176,7 @@ class Engine:
             # Rooms needed
             # - level 1+ - Devan
             # - Victory screen - Devan
-            # - opening screen - Manoell
+            # - opening screen - Manoel
             # - save/load menu - Richard
 
         class Camera:
@@ -90,5 +187,11 @@ def main():
     game = Engine()
     game.start()
 
-main()
+#main()
+
+#==============================INITIALIATION==================================
+if __name__ == '__main__':
+    root.mainloop()
+
+
 # %%
